85ab3ed473f9205a31761fa77033927e26937a20
Dasqez <dave.pytel@gmail.com>
2026-06-12 14:21:39 +0200

    Update and rename .eleventy.js.js to .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,19 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("admin");
   eleventyConfig.addPassthroughCopy("moje-foto.jpeg");
-
+  
+  // To jest ten brakujący filtr daty:
   eleventyConfig.addFilter("dateFilter", function(dateVal) {
+    if (!dateVal) return "";
     return new Date(dateVal).toLocaleDateString('pl-PL', {
       year: 'numeric', month: 'long', day: 'numeric'
     });
   });
 
-  return { dir: { input: ".", output: "_site" } };
+  return {
+    dir: {
+      input: ".",
+      output: "_site"
+    }
+  };
 };
