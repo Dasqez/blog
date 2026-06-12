479605805bf928b777f81ddd05149fea94f3376e
Dasqez <dave.pytel@gmail.com>
2026-06-12 15:39:18 +0200

    Update eleventy.config.js

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,6 +2,10 @@ export default function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("admin");
   eleventyConfig.addPassthroughCopy("moje-foto.jpeg");
 
+  module.exports = function(eleventyConfig) {
+  // Ta linijka jest kluczowa:
+  eleventyConfig.addPassthroughCopy("images");
+
   // AUTOMATYCZNE TWORZENIE KOLEKCJI Z FOLDERU _POSTS
   eleventyConfig.addCollection("posts", function(collectionApi) {
     return collectionApi.getFilteredByGlob("_posts/*.md");
